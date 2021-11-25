38e8128a33b4f3a6c76f5d2992db9ccb2eb3c036
Yashasvi Sinha <yashasvi.sinha@attainu.com>
2021-11-25 13:12:24 +0530

    added build specific stuff

--- back-end/index.js
+++ back-end/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 const PORT = process.env.PORT || 4000
 const app = express();
@@ -16,9 +17,26 @@ const allProducts = [
 
 //your all the other codes
 
+//These are required if using type as module in package.json in server code
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+
+// Have Node serve the files for our built React app
+const reactBuildPath = path.resolve(__dirname, '../front-end/build')
+app.use(express.static(reactBuildPath));
 
 app.get('/api/products', (req, res) => {
     res.json(allProducts)
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../front-end/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
